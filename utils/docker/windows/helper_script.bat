7daf10338fe987840c0b170c9a183e5fcf431fa2
Snider <snider@lt.hn>
2021-11-23 12:28:37 +0000

    removes blockchain code. (moved)

--- utils/docker/windows/helper_script.bat
+++
@@ -1,6 +0,0 @@
-
-REM This file is neccessary to set up MSYS2 environment in container.
-
-C:\msys64\usr\bin\bash  -c "/c/msys64/usr/bin/pacman-key --init"
-C:\msys64\usr\bin\bash  -c "/c/msys64/usr/bin/pacman-key --refresh-keys"
-C:\msys64\usr\bin\bash  -c "/c/msys64/usr/bin/pacman-key --populate msys2"
