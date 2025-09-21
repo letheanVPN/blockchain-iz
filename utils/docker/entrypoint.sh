6b4b1093705db79c03a3b7d03046f4f74c848be7
Snider <snider@lt.hn>
2025-09-21 15:09:23 +0100

    Remove seed_node_ip from entrypoint.sh
    Removed seed_node_ip variable and its usage in letheand command.

--- utils/docker/entrypoint.sh
+++ utils/docker/entrypoint.sh
@@ -8,7 +8,6 @@ sh|bash)
 
 letheand)
   shift
-  seed_node_ip=$(getent hosts $SEED_NODE | awk '{ print $1 }' | head -n 1)
   if tty | grep -q /dev/;
   then
     interactive=""
@@ -17,11 +16,9 @@ letheand)
   fi
   echo letheand $interactive --confirm-external-bind --data-dir=/home/lethean/.letheand \
     --p2p-bind-ip=0.0.0.0 \
-    --add-peer $seed_node_ip --seed-node $seed_node_ip \
     --log-level=$LOG_LEVEL "$@" >&2
   letheand $interactive --confirm-external-bind --data-dir=/home/lethean/.letheand \
     --p2p-bind-ip=0.0.0.0 \
-    --add-peer $seed_node_ip --seed-node $seed_node_ip \
     --log-level=$LOG_LEVEL "$@" >&2
   ;;
 
